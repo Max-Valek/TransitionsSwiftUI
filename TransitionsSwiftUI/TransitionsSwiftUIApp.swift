4e92cb09fef20702a8351219968b12948e1fe499
Max Valek <valek.maxwell@gmail.com>
2023-04-07 14:12:14 -0500

    Initial Commit

---
+++ TransitionsSwiftUI/TransitionsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionsSwiftUIApp.swift
+//  TransitionsSwiftUI
+//
+//  Created by Max Valek on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TransitionsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
